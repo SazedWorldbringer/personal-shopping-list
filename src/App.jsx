f27b9b64aca0eac77f51d6bf5d31fa66a1a50457
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-23 23:01:35 +0530

    feat: initial setup according to the book

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Routes } from "react-router-dom"
+import { Route } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
+import styled, { createGlobalStyle } from "styled-components"
+import Header from "./components/Header/Header"
+import ListDetail from "./pages/ListDetail"
+import ListForm from "./pages/ListForm"
+import Lists from "./pages/Lists"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
+
+const AppWrapper = styled.div`
+  text-align: center;
+`
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <GlobalStyle />
+      <AppWrapper>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Lists />} />
+            <Route path="/list/:listId/new" element={<ListForm />} />
+            <Route path="/list/:listId" element={<ListDetail />} />
+          </Routes>
+        </BrowserRouter>
+      </AppWrapper>
+    </>
   )
 }
 
